2e1df1f2285081e9acb84ed07256a157923474e7
Laura Gisela Giorgetta <laura.g.giorgetta@gmail.com>
2021-01-08 00:20:36 -0300

    Cambio el index a php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YilawTec</title>
+</head>
+
+<body>
+    <?php
+        include 'nav.php';
+
+    ?>
+    <main>
+        <nav>
+            <ul>
+                <li><a href="#">Estructura básica</a></li>
+            </ul>
+        </nav>
+
+    </main>
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>YilawTec</h1>
+    <nav>
+        <ul>
+            <li><a href="#">HTML y CSS</a></li>
+            <li><a href="#">Videos</a></li>
+        </ul>
+    </nav>
+</header>
